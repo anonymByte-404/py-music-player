17b8847916704fc3c34c17b8ab0db091b6e0eeb1
anonymByte-404 <an0nymnx@outlook.com>
2025-01-21 22:05:03 -0500

    refactor: add exception handling in main function

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
 from tkinterdnd2 import TkinterDnD
-import tkinter as tk
 from gui import create_gui
 from player import Player
 
 def main():
   """Initialize and run the music player application."""
-  root = TkinterDnD.Tk()
-  player = Player()
-  create_gui(root, player)
-  root.mainloop()
+  try:
+    root = TkinterDnD.Tk()
+    player = Player()
+    create_gui(root, player)
+    root.mainloop()
+  except Exception as e:
+    print(f"An error occured: {e}")
 
 if __name__ == "__main__":
   main()
