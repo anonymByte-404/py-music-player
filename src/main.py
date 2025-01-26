39e658120b21465dcc81cfa0b7180c323efe09a6
anonymByte-404 <an0nymnx@outlook.com>
2025-01-25 23:01:19 -0500

    style: fix indentation to 2 spaces in main.py

--- src/main.py
+++ src/main.py
@@ -6,26 +6,26 @@ from player import Player
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 def initialize_application() -> tuple:
-    """Initialize the main components of the application: the root window and player instance."""
-    logging.info("Initializing the application...")
-    root = TkinterDnD.Tk()
-    player = Player()
-    return root, player
+  """Initialize the main components of the application: the root window and player instance."""
+  logging.info("Initializing the application...")
+  root = TkinterDnD.Tk()
+  player = Player()
+  return root, player
 
 def run_application(root, player):
-    """Set up the GUI and run the application."""
-    logging.info("Setting up the GUI...")
-    create_gui(root, player)
-    logging.info("Starting the main application loop.")
-    root.mainloop()
+  """Set up the GUI and run the application."""
+  logging.info("Setting up the GUI...")
+  create_gui(root, player)
+  logging.info("Starting the main application loop.")
+  root.mainloop()
 
 def main():
-    """Initialize and run the music player application."""
-    try:
-        root, player = initialize_application()
-        run_application(root, player)
-    except Exception as e:
-        logging.error(f"An error occurred: {e}", exc_info=True)
+  """Initialize and run the music player application."""
+  try:
+    root, player = initialize_application()
+    run_application(root, player)
+  except Exception as e:
+    logging.error(f"An error occurred: {e}", exc_info=True)
 
 if __name__ == "__main__":
-    main()
+  main()
